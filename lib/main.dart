c8f23ea77c46cd20d7572d43bfab380f9c3e9be9
Shiel <rhezielabad3@gmail.com>
2025-08-07 23:19:01 +0800

    feat(Shiel): add ClassName with properties and  method

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,8 @@
-import 'package:flutter/material.dart';
+// File: lib/main.dart
+
+import 'package:oop_classes/shiel.dart'; // Import the Shiel class
 
 void main() {
-  runApp(const MainApp());
-}
-
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
-    );
-  }
+  Shiel myShiel = Shiel('Husay', 100);
+  myShiel.describe();
 }
