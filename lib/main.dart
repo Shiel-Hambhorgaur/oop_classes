bb03da649731320c89b857c2cd6e5f27d6e5abf5
Shiel <rhezielabad3@gmail.com>
2025-08-09 21:04:38 +0800

    feat(Shiel): add Shiel with properties and  method

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 // File: lib/main.dart
 
-import 'package:oop_classes/shiel.dart'; // Import the Shiel class
+import 'shiel.dart'; // Import the Shiel class
 
 void main() {
   Shiel myShiel = Shiel('Husay', 100);
