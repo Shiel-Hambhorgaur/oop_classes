4ce754b754ac840fdc6571988de3a800d8bf0692
Shiel <rhezielabad3@gmail.com>
2025-08-09 21:27:18 +0800

    feat(Person) add Person with properties and method

--- lib/main.dart
+++ lib/main.dart
@@ -3,16 +3,7 @@ import 'package:oop_classes/shiel.dart'; // Import the Shiel class
 
 void main() {
   // Code from feature/Yonexjaira
-  var person = Person(
-    name: 'Yonexjaira',
-    age: 19,
-    occupation: 'Tambay',
-  );
-
-  person.describe();
-  print('Life stage: ${person.getLifeStage()}');
-  person.celebrateBirthday();
-
+  var person = Person(name: 'Yonexjaira', age: 19, occupation: 'Tambay');git
   // Code from main branch
   Shiel myShiel = Shiel('Husay', 100);
   myShiel.describe();
