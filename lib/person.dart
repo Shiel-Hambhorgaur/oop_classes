4a85fa9172539010ac328037be4bd701204f72ab
Shiel <rhezielabad3@gmail.com>
2025-08-09 19:27:54 +0800

    main.dart: add Main with properties and method

--- lib/person.dart
+++ lib/person.dart
@@ -1,14 +1,10 @@
 // lib/person.dart
 class Person {
   String name;
-  int age; 
+  int age;
   String occupation;
 
-  Person({
-    required this.name,
-    required this.age,
-    required this.occupation,
-  });
+  Person({required this.name, required this.age, required this.occupation});
 
   void describe() {
     print('$name is $age years old and works as a $occupation.');
