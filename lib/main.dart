30b58668d2c5349633ff2dd40ab23c54b1ca2d5c
Shiel <rhezielabad3@gmail.com>
2025-08-10 19:49:59 +0800

    Renamed Yonez.dart to name.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,8 @@
+// File: lib/main.dart
+
 import 'Yonez.dart';
-import 'package:oop_classes/shiel.dart'; // Import the Shiel class
 
 void main() {
-  // Code from feature/Yonexjaira
-  var person = Person(name: 'Yonexjaira', age: 19, occupation: 'Tambay');git
-  // Code from main branch
-  Shiel myShiel = Shiel('Husay', 100);
-  myShiel.describe();
+  var person = Person(name: 'Yonexjaira', age: 19, occupation: 'Tambay');
+  person.describe();
 }
