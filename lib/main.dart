5a65aacdd9f11ddf0d47ac09e0a8863646bb91d1
Shiel <rhezielabad3@gmail.com>
2025-08-10 20:24:04 +0800

    feat (Name): add Name with properties and method

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,4 @@
 import 'arts_2025.dart';
-import 'Yonez.dart';
-import 'package:oop_classes/shiel.dart'; // Import the Shiel class
 
 void main() {
   // Code from feature/arts-2025
@@ -10,18 +8,4 @@ void main() {
   DateTime today = DateTime.now();
   print("Date: ${today.toLocal()}");
 
-  // Code from feature/Yonexjaira
-  var person = Person(
-    name: 'Yonexjaira',
-    age: 19,
-    occupation: 'Tambay',
-  );
-
-  person.describe();
-  print('Life stage: ${person.getLifeStage()}');
-  person.celebrateBirthday();
-
-  // Code from main branch
-  Shiel myShiel = Shiel('Husay', 100);
-  myShiel.describe();
 }
